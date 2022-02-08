4835186155c1d31c5b2627bb6e94f9018bc23e83
moises <moisesnueva@gmail.com>
2022-02-08 20:26:28 +0100

    Changes to improve the performance of the app

--- src/main/java/com/challenge/gamerps/controllers/RPSRestController.java
+++ src/main/java/com/challenge/gamerps/controllers/RPSRestController.java
@@ -1,5 +1,7 @@
 package com.challenge.gamerps.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.challenge.gamerps.bean.GameDataBean;
 import com.challenge.gamerps.services.GameDataService;
@@ -29,10 +33,21 @@ public class RPSRestController {
 		return "playingpage";
 	}
 	
-	@PostMapping(value = "/showdata", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public String showDataPage(@RequestBody GameDataBean[] dataBean, Model model) {
-		model.addAttribute("data", gameDataService.getTotalData(dataBean));
-		model.addAttribute("rounds", dataBean.length);
+	@PostMapping(value = "/postdata", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public List<List<Object>> postData(@RequestBody GameDataBean[] dataBean, Model model) {
+		return gameDataService.getTotalData(dataBean);
+	}
+	
+	@GetMapping(value = "/showdata")
+	public String showDataPage(@RequestParam(value="firsts", defaultValue="0") String firsts,
+			@RequestParam(value="seconds", defaultValue="0") String seconds,
+			@RequestParam(value="draws", defaultValue="0") String draws, 
+			@RequestParam(value="rounds", defaultValue="0") String rounds, Model model) {
+		model.addAttribute("firsts", firsts);
+		model.addAttribute("seconds", seconds);
+		model.addAttribute("draws", draws);
+		model.addAttribute("rounds", rounds);
 		return "datapage";
 	}
 	
